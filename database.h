6a780927ae0e35fb80cf6b927d7afe16ab3df851
Tomáš Batelka <tomas.batelka@email.cz>
2022-04-19 01:30:15 +0200

    Už nevim co

--- database.h
+++ database.h
@@ -5,5 +5,6 @@ void print_all();
 void export_to_csv(FILE *output);
 void add_album(struct Album *album);
 void remove_album(int index);
+void sort_albums(Order order);
 
 #endif // DATABASE_H_INCLUDED
